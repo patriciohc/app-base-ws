672ca4aa7ae7e77068e0b482fa85fe30a314a902
Jose Patricio Hijuitl Cuatlayo <Hijuitljp@MBP-de-soporte.lan>
2017-09-05 11:11:46 -0500

    configuracion cleardb

--- conection_db.js
+++ conection_db.js
@@ -4,10 +4,14 @@ const mysql = require('mysql');
 
 var isConected = false;
 var conecction = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database : 'base_ws',
+    // host: 'localhost',
+    // user: 'root',
+    // password: '',
+    // database : 'base_ws',
+    host: 'us-cdbr-iron-east-05.cleardb.net',
+    user: 'b93a567617aa59',
+    password: '619b8316',
+    database : 'heroku_e41bcc31c05226c',
 });
 
 function connect() {
