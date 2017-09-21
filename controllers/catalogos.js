9771b8b91ffb2e19079f7a1448e50883f6d48623
Jose Patricio Hijuitl Cuatlayo <Hijuitljp@MacBook-Pro-de-soporte.local>
2017-09-20 23:15:42 -0500

    catalogos

--- controllers/catalogos.js
+++ controllers/catalogos.js
@@ -5,6 +5,7 @@ function get (req, res) {
   fs.readFile('../public/json/catalogos.json', 'utf8', function (err, data) {
     if (err) {
       return res.status(200).send({err})
+      console.log(err) 
     }
     var obj = JSON.parse(data);
     return res.status(200).send(obj)
