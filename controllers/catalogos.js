d04061eb78cca75d0cd4be0109d9adb2f2a66f5c
Jose Patricio Hijuitl Cuatlayo <Hijuitljp@MacBook-Pro-de-soporte.local>
2017-09-20 23:29:28 -0500

    catalogos

--- controllers/catalogos.js
+++ controllers/catalogos.js
@@ -1,15 +1,26 @@
 'use strict'
 var fs = require('fs');
 
+CATALOGOS = {
+  "estatus": [
+    "En espera de aceptación",
+    "Aceptado por la tienda",
+    "En ruta",
+    "Entregado",
+  ]
+}
+
 function get (req, res) {
-  fs.readFile('../public/json/catalogos.json', 'utf8', function (err, data) {
-    if (err) {
-      return res.status(200).send({err})
-      console.log(err) 
-    }
-    var obj = JSON.parse(data);
-    return res.status(200).send(obj)
-  });
+  // fs.readFile('../public/json/catalogos.json', 'utf8', function (err, data) {
+  //   if (err) {
+  //     return res.status(200).send({err})
+  //     console.log(err)
+  //   }
+  //   var obj = JSON.parse(data);
+  //   return res.status(200).send(obj)
+  // });
+  return res.status(200).send(CATALOGOS)
+
 }
 
 module.exports = {
