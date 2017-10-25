a52aea485a39a61294b4c962848c2d41fa476a98
Jose Patricio Hijuitl Cuatlayo <Hijuitljp@MBP-de-soporte.lan>
2017-10-25 18:58:57 -0500

    test

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const bodyParser = require('body-parser');
 var cors = require('cors');
 
 var app = express();
+app.use(express.static(__dirname+"/public"));
 // websockets
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
