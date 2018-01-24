bc625ed1e022f9c04f91c534b1aae642689b6fc8
Jose Patricio Hijuitl Cuatlayo <Hijuitljp@MacBook-Pro-de-soporte.local>
2018-01-23 22:34:57 -0600

    correcion cionexion base de datos

--- conection_db.js
+++ conection_db.js
@@ -3,15 +3,12 @@
 const mysql = require('mysql');
 
 var isConected = true;
-var conecction = mysql.createConnection({
-   host: 'localhost',
-   user: 'root',
-   password: '',
-   database : 'base_ws',
-    host: 'us-cdbr-iron-east-05.cleardb.net',
-    user: 'b93a567617aa59',
-    password: '619b8316',
-    database : 'heroku_e41bcc31c05226c?reconnect=true',
+var conecction  = mysql.createPool({
+    connectionLimit : 5,
+    host            : 'us-cdbr-iron-east-05.cleardb.net',
+    user            : 'b93a567617aa59',
+    password        : '619b8316',
+    database        : 'heroku_e41bcc31c05226c'
 });
 
 // var conecction  = mysql.createPool({
